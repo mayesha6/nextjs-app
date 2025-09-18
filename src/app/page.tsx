81d97b0f30f01e1825ad176cdca7276e029fed72
mayesha <soumyrhmn234@gmail.com>
2025-09-18 09:27:33 +0600

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client"
 import Counter from "@/components/Counter";
+import { useRouter } from "next/navigation";
 
 const HomePage = () => {
+  const router = useRouter()
+  const handleNavigation = () => {
+    router.push("/dashboard")
+  }
   return (
     <div>
       <h1 className="text-5xl text-center text-bold py-4">This is home page.</h1>
       <Counter/>
+      <button onClick={handleNavigation} className="bg-blue-600 text-white font-semibold mx-auto p-3 rounded-lg block my-5">Dashboard</button>
     </div>
   );
 };
