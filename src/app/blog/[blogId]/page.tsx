36f899b3c4861c7f7c51c5844eb46ccff59a60aa
mayesha <soumyrhmn234@gmail.com>
2025-09-18 07:11:21 +0600

    create blog details page using dynamic routing

---
+++ src/app/blog/[blogId]/page.tsx
@@ -0,0 +1,14 @@
+const BlogDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+  return (
+    <div>
+      <h1>This is dynamic blog details page. blog id : {blogId}</h1>
+    </div>
+  );
+};
+
+export default BlogDetailsPage;
