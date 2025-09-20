2613743bd66405a89a9e19b5e9470c487c7528e2
mayesha <soumyrhmn234@gmail.com>
2025-09-20 08:01:34 +0600

    fetch cache

--- src/app/(commonLayout)/blog/page.tsx
+++ src/app/(commonLayout)/blog/page.tsx
@@ -2,21 +2,21 @@ import BlogCard from "@/components/BLogCard";
 import { IBlog } from "@/type";
 
 const BlogPage = async () => {
-    const res = await fetch("http://localhost:3004/blogs")
-    const blogs = await res.json()
-    // console.log(blogs)
-    return (
-        <div className="container mx-auto">
-            <h1 className="text-5xl text-center text-bold py-4">this is blog page</h1>
-            <div className="grid grid-cols-3 my-16 gap-4 justify-between">
-                {
-                blogs.map((blog: IBlog)=>(
-                    <BlogCard key={blog.id} blog={blog}/>
-                ))
-            }
-            </div>
-        </div>
-    );
+  const res = await fetch("http://localhost:3004/blogs", {
+    cache: "force-cache",
+  });
+  const blogs = await res.json();
+  // console.log(blogs)
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-5xl text-center text-bold py-4">this is blog page</h1>
+      <div className="grid grid-cols-3 my-16 gap-4 justify-between">
+        {blogs.map((blog: IBlog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogPage;
