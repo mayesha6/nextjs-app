b817b25b77876af42f9e5a103952cf4cae782905
mayesha <soumyrhmn234@gmail.com>
2025-09-20 08:24:26 +0600

    create blog loading page

---
+++ src/app/(commonLayout)/blog/loading.tsx
@@ -0,0 +1,10 @@
+
+const LoadingBlog = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-center">Loading...</h1>
+        </div>
+    );
+};
+
+export default LoadingBlog;
